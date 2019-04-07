3a74750d7ec66e5846969c604f998efc0281b8c5
Josuah Aron <josuah.aron@gmail.com>
2019-04-07 19:11:33 +0200

    Update plot_regression_3d.py
    Avoid using same value for both input features.

--- plot_regression_3d.py
+++ plot_regression_3d.py
@@ -4,31 +4,29 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import linear_model
 
 
-def train_and_make_prediction(x1, x2, y):
-    x = numpy.array(list(zip(x1, x2)))
-    #
-    x_train = x[:-25]
-    y_train = numpy.array(y[:-25]).reshape(-1, 1)
-    # Create linear regression object
-    model = linear_model.LinearRegression()
-    # Train the model using the training sets
-    model.fit(x_train, y_train)
-    return model.predict(x)
-
-
-a, b, c, d = -1, 1, 2, -4
-x = numpy.linspace(0, 50, 50)
-y = numpy.linspace(0, 50, 50)
-z = -1/c * (a*x + b*y + d)
-
+# generate data
 numpy.random.seed(101)
-feature1 = x + numpy.random.uniform(-4, 4, 50)
-feature2 = y + numpy.random.uniform(-4, 4, 50)
-target = z + numpy.random.uniform(-4, 4, 50)
+a, b, c, d = -1, 1, 2, -4
+x1 = numpy.linspace(0, 50, 50) + numpy.random.uniform(-4, 4, 50)
+numpy.random.seed(1983)
+x2 = numpy.linspace(0, 50, 50) + numpy.random.uniform(-4, 4, 50)
+y = -1/c * (a*x + b*y + d)
 
-# Make predictions using the testing set
-z_pred = train_and_make_prediction(feature1, feature2, target)
+# transform inputs into 2D array
+x = numpy.array(list(zip(x1, x2)))
 
+# create a training set
+x_train = x[:-25]
+y_train = numpy.array(y[:-25]).reshape(-1, 1)
+
+# train linear regression object
+model = linear_model.LinearRegression()
+model.fit(x_train, y_train)
+
+# make predictions
+z_pred = model.predict(x)
+
+# plot the solution
 fig = pyplot.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -37,11 +35,13 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-surf = ax.plot_surface(x, y, z_pred,
+# dataset
+ax.scatter(x1, x2, y, c='r')
+
+# prediction
+surf = ax.plot_surface(x1, x2, z_pred,
                        alpha=0.5,
                        linewidth=0,
                        antialiased=False)
-
-ax.scatter(feature1, feature2, target, c='r')
 
 pyplot.show()
